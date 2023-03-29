9f91d5690771bdab5931454fc937758e50ee03f1
Shivam <shivam.yadav98939294@gmail.com>
2023-03-29 22:39:31 +0530

    done

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const home = document.getElementById("home");
 const load = document.getElementById("load");
 const home1 = document.getElementById("home-1");
 const crop_container = document.getElementById("crop-container");
-let imageObj;
 var actionButton = document.querySelectorAll(".action-button button");
-
+let imageObj;
+//function to handle image uploaded and then cropping it
 fileInput.onchange = () => {
   let selectedFile = fileInput.files[0];
 
@@ -22,18 +22,22 @@ fileInput.onchange = () => {
   var image_workspace = document.querySelector(".image-workspace img");
 
   var file = selectedFile;
+  // creating url of  object image file
   var url = window.URL.createObjectURL(new Blob([file], { type: "image/jpg" }));
   image_workspace.src = url;
   image_workspaceSpan.style.display = "none";
-  fileInput.value = '';
+  fileInput.value = "";
+
+  //Cropper options
+
   var options = {
     dragMode: "move",
     preview: ".img-preview",
     viewMode: 2,
     modal: false,
     background: false,
+    // cropping 
     ready: function () {
-      // zoom for image
       // rotate image
       rotate[0].onclick = () => cropper.rotate(90);
       rotate[1].onclick = () => cropper.rotate(-90);
@@ -50,24 +54,22 @@ fileInput.onchange = () => {
         flipY = -flipY;
       };
 
-      // download cropped image
+      // After image is croped sending it to frame section and then applying frames
       actionButton[0].onclick = () => {
         actionButton[0].innerText = "...";
         cropper.getCroppedCanvas().toBlob((blob) => {
-          var downloadUrl = window.URL.createObjectURL(blob);
-          //var a = document.createElement("a");
-         // a.href = downloadUrl;
-          selectedFile=blob;
-          //a.download = "cropped-image.jpg"; // output image name
-          // a.click()
-          actionButton[0].innerText = "Crop";
+          // here blob is cropped image object assigning it to selectedfile
+          selectedFile = blob;
 
+          actionButton[0].innerText = "Crop";
+          // displaying frame section and hiding crop section
           document.querySelector("#crop-container").style.display = "none";
-           document.querySelector("#home-1").style.display = "flex";
-         document.querySelector("#hide").style.display = "block";
-         document.querySelectorAll(".container-after")[0].style.display =   "flex";
-    
-         document.querySelector(
+          document.querySelector("#home-1").style.display = "flex";
+          document.querySelector("#hide").style.display = "block";
+          document.querySelectorAll(".container-after")[0].style.display =
+            "flex";
+          // adding cropped image to frame section so that frame can be applied on it
+          document.querySelector(
             ".image-workspace-after"
           ).innerHTML = `<img src="" alt="">`;
           var image_workspace = document.querySelector(
@@ -75,14 +77,15 @@ fileInput.onchange = () => {
           );
 
           var file = selectedFile;
+          // creating url of  object image file
           var url = window.URL.createObjectURL(
             new Blob([file], { type: "image/jpg" })
           );
           image_workspace.src = url;
-          document.querySelector(".download-after").onclick=()=>{
-            putframe(selectedFile);
-          }
-         
+          document.querySelector(".after-frame-bt").onclick = () => {
+            afterPuttingFrame(selectedFile);
+          };
+
           // document.querySelector('.image-workspace-after span').style.display = 'none'
         });
       };
@@ -90,39 +93,33 @@ fileInput.onchange = () => {
   };
 
   var cropper = new Cropper(image_workspace, options);
-
-  //   fileupload.appendChild(img);
 };
-function putframe(selectedFile) {
-  document.querySelector(
-    "#img-div"
-  ).innerHTML = `<img src="" alt="">`;
-  var image_workspace = document.querySelector(
-    "#img-div img"
-  );
 
-  var file = selectedFile;
-  var url = window.URL.createObjectURL(
-    new Blob([file], { type: "image/jpg" })
-  );
-  image_workspace.src = url;
-  var mask=  $('.image-workspace-after img').css('-webkit-mask-box-image')
-  $('#img-div img').css('-webkit-mask-box-image',mask)
- 
-//   const el=document.querySelector(".image-workspace-after")
-  
-// const clone = el.cloneNode(true);
-//   document.querySelector("#img-div").appendChild(clone);
-  document.querySelector("#hide").style.display = "none";
-  document.querySelectorAll(".container-after")[0].style.display =   "none";
-
-}
+// function to handle file uploading
 fileButton.addEventListener("click", (e) => {
   if (fileInput) {
+    // now calling file input to handle file uploaded
     fileInput.click();
   }
   e.preventDefault();
 });
+
+//final function for adding image to main page
+function afterPuttingFrame(selectedFile) {
+  // Adding the croped image with frame and hiding frame section , container after crop
+  document.querySelector("#img-div").innerHTML = `<img src="" alt="">`;
+  var image_workspace = document.querySelector("#img-div img");
+
+  var file = selectedFile;
+  var url = window.URL.createObjectURL(new Blob([file], { type: "image/jpg" }));
+  image_workspace.src = url;
+  var mask = $(".image-workspace-after img").css("-webkit-mask-box-image");
+  $("#img-div img").css("-webkit-mask-box-image", mask);
+  document.querySelector("#hide").style.display = "none";
+  document.querySelectorAll(".container-after")[0].style.display = "none";
+}
+
+// Applying frames
 
 const frames1 = document.querySelector("#fr1");
 const frames2 = document.querySelector("#fr2");
@@ -131,47 +128,51 @@ const frames4 = document.querySelector("#fr4");
 const frames5 = document.querySelector("#fr5");
 
 frames1.onclick = () => {
-  $('.image-workspace-after img').css('-webkit-mask-box-image',"url('./assets/user_image_frame_1.png')")
- 
+  $(".image-workspace-after img").css(
+    "-webkit-mask-box-image",
+    "url('./assets/user_image_frame_1.png')"
+  );
 };
 frames2.onclick = () => {
-  $('.image-workspace-after img').css('-webkit-mask-box-image',"url('./assets/user_image_frame_2.png')")
- 
- };
- frames3.onclick = () => {
-  $('.image-workspace-after img').css('-webkit-mask-box-image',"url('./assets/user_image_frame_3.png')")
- 
- };
- frames4.onclick = () => {
-  $('.image-workspace-after img').css('-webkit-mask-box-image',"url('./assets/user_image_frame_4.png')")
- 
- };
- frames5.onclick = () => {
-  $('.image-workspace-after img').css('-webkit-mask-box-image',"")
- 
- };
+  $(".image-workspace-after img").css(
+    "-webkit-mask-box-image",
+    "url('./assets/user_image_frame_2.png')"
+  );
+};
+frames3.onclick = () => {
+  $(".image-workspace-after img").css(
+    "-webkit-mask-box-image",
+    "url('./assets/user_image_frame_3.png')"
+  );
+};
+frames4.onclick = () => {
+  $(".image-workspace-after img").css(
+    "-webkit-mask-box-image",
+    "url('./assets/user_image_frame_4.png')"
+  );
+};
+frames5.onclick = () => {
+  $(".image-workspace-after img").css("-webkit-mask-box-image", "");
+};
+
+// Back arrow section 
+document.querySelector("#left-arrow-div").onclick = () => {
+  window.location.reload();
+};
+document.querySelector("#left-arrow-crop").onclick = () => {
+  document.querySelector("#crop-container").style.display = "none";
+  document.querySelector("#home-1").style.display = "flex";
+};
+document.querySelector("#cancel").onclick = () => {
+  document.querySelectorAll(".container-after")[0].style.display = "none";
+  document.querySelector("#home-1").style.display = "flex";
+};
+
+//loading animation
 
 window.onload = () => {
-  // document.querySelector('.container-after').style.display="none"
   setTimeout(() => {
     home.style.display = "block";
     load.style.display = "none";
   }, 1000);
 };
-document.querySelector("#left-arrow-div").onclick=()=>{
-  
-  window.location.reload()
-};
-document.querySelector("#left-arrow-crop").onclick=()=>{
-  document.querySelector("#crop-container").style.display = "none";
-  document.querySelector("#home-1").style.display = "flex";
-};
-document.querySelector("#cancel").onclick=()=>{
-  document.querySelectorAll(".container-after")[0].style.display =   "none";
-  document.querySelector("#home-1").style.display = "flex";
-    
-};
-// upload image
-// actionButton[0].onclick = () => hiddenUpload.click()
-// hiddenUpload.onchange = () => {
-//     // apdate on new file selected issue removed here
---
+++ readme.md
@@ -0,0 +1,16 @@
+
+My(developer) details 
+Email:shivam.yadav98939294@gmail.com
+
+
+About this web-app:-
+
+1:- Used various svg icons for flipping rotating and for going back and canceling cropping.
+
+2:- For cropping the image i have used cropper library of javascript. 
+
+3:- css mask property for applying frames.
+
+4:-Sometime during applying frames i needed to use  jquery for masking cropped image and applying frames.
+
+5:-I have used comments so that anyone who reads my code can understand it easily.
